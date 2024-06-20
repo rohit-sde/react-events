31f9328d0a65529ae5bf1abc4b1f98485eb3143d
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-20 20:37:00 +0530

    added querry for (live updating data)

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,52 @@
 import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from 'react-router-dom';
+    Navigate,
+    RouterProvider,
+    createBrowserRouter,
+} from "react-router-dom";
 
-import Events from './components/Events/Events.jsx';
-import EventDetails from './components/Events/EventDetails.jsx';
-import NewEvent from './components/Events/NewEvent.jsx';
-import EditEvent from './components/Events/EditEvent.jsx';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import Events from "./components/Events/Events.jsx";
+import EventDetails from "./components/Events/EventDetails.jsx";
+import NewEvent from "./components/Events/NewEvent.jsx";
+import EditEvent from "./components/Events/EditEvent.jsx";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Navigate to="/events" />,
-  },
-  {
-    path: '/events',
-    element: <Events />,
+    {
+        path: "/",
+        element: <Navigate to="/events" />,
+    },
+    {
+        path: "/events",
+        element: <Events />,
 
-    children: [
-      {
-        path: '/events/new',
-        element: <NewEvent />,
-      },
-    ],
-  },
-  {
-    path: '/events/:id',
-    element: <EventDetails />,
-    children: [
-      {
-        path: '/events/:id/edit',
-        element: <EditEvent />,
-      },
-    ],
-  },
+        children: [
+            {
+                path: "/events/new",
+                element: <NewEvent />,
+            },
+        ],
+    },
+    {
+        path: "/events/:id",
+        element: <EventDetails />,
+        children: [
+            {
+                path: "/events/:id/edit",
+                element: <EditEvent />,
+            },
+        ],
+    },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
