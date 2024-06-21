75008aa002bfc0c9354ca76417c952ee612a0bef
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-21 18:18:18 +0530

    added delete confirmation

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -3,8 +3,12 @@ import { Link, Outlet, useParams, useNavigate } from "react-router-dom";
 import Header from "../Header.jsx";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
+import { useState } from "react";
+import Modal from "../UI/Modal.jsx";
 
 export default function EventDetails() {
+    const [isDeleting, setIsDeleting] = useState();
+
     const params = useParams();
     const navigate = useNavigate();
     const { data, isPending, isError, error } = useQuery({
@@ -22,6 +26,14 @@ export default function EventDetails() {
             navigate("/events");
         },
     });
+
+    function handleStartDelete() {
+        setIsDeleting(true);
+    }
+
+    function handleStopDelete() {
+        setIsDeleting(false);
+    }
 
     function handleDelete() {
         mutate({ id: params.id });
@@ -63,7 +75,7 @@ export default function EventDetails() {
                 <header>
                     <h1>{data.title}</h1>
                     <nav>
-                        <button onClick={handleDelete}>Delete</button>
+                        <button onClick={handleStartDelete}>Delete</button>
                         <Link to="edit">Edit</Link>
                     </nav>
                 </header>
@@ -87,6 +99,26 @@ export default function EventDetails() {
     }
     return (
         <>
+            {isDeleting && (
+                <Modal onClose={handleStopDelete}>
+                    <h2>Are you sure ?</h2>
+                    <p>
+                        Do you really want to delete this event? This action
+                        cannot be undone.
+                    </p>
+                    <div className="form-acitons">
+                        <button
+                            onClick={handleStopDelete}
+                            className="button-text"
+                        >
+                            Cancel
+                        </button>
+                        <button onClick={handleDelete} className="button">
+                            Delete
+                        </button>
+                    </div>
+                </Modal>
+            )}
             <Outlet />
             <Header>
                 <Link to="/events" className="nav-item">
