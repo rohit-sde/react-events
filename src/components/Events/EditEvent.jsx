d4b521ea5989327299cdac7f576d694c7516db87
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-21 22:55:47 +0530

    get pre populated editform

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,27 +1,67 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { fetchEvent } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const params = useParams();
 
-  function handleSubmit(formData) {}
+    const { data, isPending, isError, error } = useQuery({
+        queryKey: ["events", params.id],
+        queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+    });
 
-  function handleClose() {
-    navigate('../');
-  }
+    function handleSubmit(formData) {}
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
-    </Modal>
-  );
+    function handleClose() {
+        navigate("../");
+    }
+
+    let content;
+
+    if (isPending) {
+        content = (
+            <div className="center">
+                <LoadingIndicator />
+            </div>
+        );
+    }
+
+    if (isError) {
+        content = (
+            <>
+                <ErrorBlock
+                    title="Failed to load event"
+                    message={
+                        error.info?.message ||
+                        "Failed to load event. Please check your inputs and try again later"
+                    }
+                />
+                <div className="form-actions">
+                    <Link to="../" className="button">
+                        Okay
+                    </Link>
+                </div>
+            </>
+        );
+    }
+
+    if (data) {
+        content = (
+            <EventForm inputData={data} onSubmit={handleSubmit}>
+                <Link to="../" className="button-text">
+                    Cancel
+                </Link>
+                <button type="submit" className="button">
+                    Update
+                </button>
+            </EventForm>
+        );
+    }
+    return <Modal onClose={handleClose}>{content}</Modal>;
 }
